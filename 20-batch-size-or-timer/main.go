d5b07e0236aba4fb4d45f4296de084b94a74414d
Jose Juan Montiel <josejuan.montiel@gmail.com>
2021-10-21 22:29:36 +0200

    otro

--- 20-batch-size-or-timer/main.go
+++ 20-batch-size-or-timer/main.go
@@ -11,44 +11,29 @@ import (
 	"time"
 )
 
+const max = 20
+const chanSize = 2
+
+const feedMilis = 20
+const tickerMilis = 500
+const block = 5
+
+type SingleRequest struct {
+	request      int
+	responseChan chan SingleResponse
+}
+
+type SingleResponse struct {
+	response int
+}
+
 func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
 }
 
-func main() {
+func group(chanIn chan SingleRequest, chanOut chan []int) {
 
-	const max = 50
-	const chanSize = 50
-
-	const feedMilis = 5
-	const tickerMilis = 500
-	const block = 25
-
-	var done = make(chan bool)
-
-	var chanIn = make(chan int, max)
-	var chanAux = make(chan int, chanSize)
-	var chanOut = make(chan []int, chanSize)
-
-	var wait = &sync.WaitGroup{}
-
-	fmt.Println("Waiting")
-
-	go func() {
-		// Seed should be set once, better spot is func init()
-		rand.Seed(time.Now().UTC().UnixNano())
-		for i := 0; i < max; i++ {
-			wait.Add(1)
-			fmt.Println("In <-", i)
-			chanIn <- i
-			time.Sleep(time.Duration(randInt(1, feedMilis)) * time.Millisecond)
-		}
-
-		close(chanIn)
-
-		wait.Wait()
-		close(chanOut)
-	}()
+	var chanAux = make(chan SingleRequest, chanSize)
 
 	go func() {
 		ticker := time.NewTicker(tickerMilis * time.Millisecond)
@@ -56,7 +41,7 @@ func main() {
 			select {
 			case <-ticker.C:
 
-				var arr []int
+				var arr []SingleRequest
 				var howmany int
 
 				length := len(chanAux)
@@ -70,39 +55,115 @@ func main() {
 					item := <-chanAux
 					arr = append(arr, item)
 				}
-				chanOut <- arr
-			}
 
-		}
-	}()
-
-	go func() {
-		for item := range chanOut {
-			for _, _ = range item {
-				wait.Done()
+				if len(arr) > 0 {
+					//fmt.Println("Out -> tiempo o por longitud (", length >= block, ")")
+					var grouped []int
+					for _, simple := range arr {
+						simple.responseChan <- SingleResponse{response: simple.request}
+						grouped = append(grouped, simple.request)
+					}
+					chanOut <- grouped
+				}
 			}
-			fmt.Println(item, " -> Out")
 		}
-		done <- true
 	}()
 
 	go func() {
 		for item := range chanIn {
+			//fmt.Println("Reading from chanIn -> ", item.request)
+
 			chanAux <- item
 
-			var arr []int
+			var arr []SingleRequest
 			length := len(chanAux)
 			if length >= block {
 				for i := 0; i < length; i++ {
 					item := <-chanAux
 					arr = append(arr, item)
 				}
-				chanOut <- arr
+				//fmt.Println("Out -> por longitud")
+				var grouped []int
+				for simple := range arr {
+					item.responseChan <- SingleResponse{response: simple}
+					grouped = append(grouped, simple)
+				}
+				chanOut <- grouped
 			}
 
 		}
 		close(chanAux)
 	}()
+
+}
+
+func loop(iteration int, wait *sync.WaitGroup, done chan bool) {
+	var chanIn = make(chan SingleRequest, max)
+
+	var chanOut = make(chan []int, chanSize)
+
+	go func() {
+		// Seed should be set once, better spot is func init()
+		rand.Seed(time.Now().UTC().UnixNano())
+		for i := 0; i < max; i++ {
+
+			go func() {
+				// TODO Imprimir tu respuesta... tras hacer batch y recibir la respuesta y separarlas...
+				// metiendo el envio y recpcion en gorutine extra...
+
+				responseChannel := make(chan SingleResponse, 1)
+				wait.Add(1)
+				req := SingleRequest{
+					request:      i,
+					responseChan: responseChannel,
+				}
+				fmt.Println("In <- ", req.request)
+				chanIn <- req
+
+				//fmt.Println("Waiting response in controller")
+				respFromChannel := <-responseChannel
+				close(responseChannel)
+
+				fmt.Println(respFromChannel, " -> End Controller")
+			}()
+
+			time.Sleep(time.Duration(randInt(1, feedMilis)) * time.Millisecond)
+		}
+		close(chanIn)
+
+		wait.Wait()
+		close(chanOut)
+	}()
+
+	group(chanIn, chanOut)
+
+	go func() {
+		for item := range chanOut {
+			for _, _ = range item {
+				// La llamada al exterior... con un batch (item es un array)
+
+				// Usar el channelWait recibido para notificar que tienes la respuesta (tras buscarla en el batch...)
+
+				// Vamos indicando que hemos procesado las respuestas...
+				wait.Done()
+			}
+			fmt.Println(item, " -> Out (", iteration, ")")
+		}
+		// TODO ... revisar esto si se llama desde LOOP...
+		done <- true
+	}()
+}
+
+func main() {
+
+	var done = make(chan bool)
+	var wait = &sync.WaitGroup{}
+
+	fmt.Println("Waiting")
+
+	for i := 0; i < 1; i++ {
+		loop(i, wait, done)
+	}
 
 	<-done
 	fmt.Println("Finish")
